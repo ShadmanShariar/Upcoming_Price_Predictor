6db7b268baa85e9542498c6be2a0c926314cad34
Shadman Shariar <75669228+ShadmanShariar@users.noreply.github.com>
2022-08-02 01:17:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import pandas
+import numpy as np
+import csv
 import matplotlib.pyplot as mp
 from sklearn.linear_model import LinearRegression
 data = pandas.read_csv\
     ("iphone_price.csv")
+
+CSVData = open("iphone_price2.csv")
+Array2d_result = np.loadtxt(CSVData, delimiter=",")
+
+# print(Array2d_result)
 print(data)
-#----------------------------------------->matplotlib.pyplot
-mp.scatter(data["version"],data["price"])
-mp.show()
+# mp.scatter(data["version"],data["price"])
+# mp.show()
 mp.plot(data["version"],data["price"])
-#mp.legend()
+mp.title("Upcoming Price Predictor")
+mp.xlabel("Model Number")
+mp.ylabel("Price")
 mp.show()
-mp.bar(data["version"],data["price"])
-mp.show()
+# mp.bar(data["version"],data["price"])
+# mp.show()
 model = LinearRegression()
-#------------------------------------------->sklearn.linear_model
 model.fit(data[["version"]],data[["price"]])
-future_model_number = 30;
-print(model.predict([[future_model_number]]))
+print("Enter Your Model Number")
+mod = int(input())
+c = 1
+ans = 0
+for i in range (len(Array2d_result)):
+    if(Array2d_result[i][0]==mod):
+       ans = int(Array2d_result[i][1])
+       c=2
+       break
+try:
+ if(c==1):
+    future_model_number = mod;
+    print("Upcoming predicted price")
+    print(int(model.predict([[future_model_number+2]])),"$")
+ else:
+    print("Upcoming predicted price")
+    print(ans,"$")
+except NameError:
+  print("")
